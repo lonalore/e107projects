a5ae6a1b4c1b8098cc3ac27732fbe8156f6f065f
lonalore <lonalore@freemail.hu>
2016-12-02 14:52:27 +0100

    OpenLayers Popup notifications.

--- js/e107projects.openlayers.js
+++ js/e107projects.openlayers.js
@@ -63,7 +63,7 @@ var e107 = e107 || {'settings': {}, 'behaviors': {}};
 			{
 				e107.mapPopupContainer.style.display = 'none';
 				e107.prologueContainer.fadeTo(500, 1);
-			}, 10000);
+			}, 1000000);
 		}
 	};
 
